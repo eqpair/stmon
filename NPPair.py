f4849e584db42812ac683c6868b5f2003eb0992d
eqpair <frmn3962@gmail.com>
2020-05-20 23:25:57 +0900

    파일나눔

---
+++ NPPair.py
@@ -0,0 +1,126 @@
+import requests
+import sqlite3
+import json
+import numpy
+from pandas import Series, DataFrame
+from bs4 import BeautifulSoup
+#from datetime import date, time, datetime, timedelta
+import time
+import datetime
+
+class NPPair:
+    def __init__(self, code1, code2, avg_period):
+        self.avg_period = avg_period
+        self.A_code = code1
+        self.B_code = code2
+        self.SL_in_val = 2
+        self.SL_out_val = 1
+        self.LS_in_val = -2
+        self.LS_out_val = -1.5
+        
+        self.dateList = []
+        self.A_closeList = []
+        self.B_closeList = []
+
+        self.SL_r = False
+        self.LS_r = False
+        self.SL_in = False
+        self.LS_in = False
+        self.SL_out = False
+        self.LS_out = False
+
+        yesterday = datetime.date.today() - datetime.timedelta(1)
+        
+        # A (보통주) 종가 가져오기
+        #req = requests.get('https://fchart.stock.naver.com/sise.nhn?symbol=%s&timeframe=day&count=260&requestType=0'%self.A_code)
+        req = requests.get('https://fchart.stock.naver.com/sise.nhn?symbol=%s&timeframe=day&startTime=%s&count=%d&requestType=0'%(self.A_code, yesterday.strftime('%Y%m%d'), avg_period+20))
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        self.A_name = soup.find('chartdata')['name']
+        for candle_element in soup.findAll('item'):
+            arr = candle_element['data'].split('|')
+            self.dateList.append(datetime.date(int(arr[0][0:4]), int(arr[0][4:6]), int(arr[0][6:9])))
+            self.A_closeList.append(int(arr[4]))
+
+        # B (우선주) 종가 가져오기
+        soup = BeautifulSoup(requests.get('https://fchart.stock.naver.com/sise.nhn?symbol=%s&timeframe=day&startTime=%s&count=%d&requestType=0'%(self.B_code, yesterday.strftime('%Y%m%d'), avg_period+20)).text, 'html.parser')
+        self.B_name = soup.find('chartdata')['name']
+        for candle_element in soup.findAll('item'):
+            self.B_closeList.append(int(candle_element['data'].split('|')[4]))
+
+        # DataFrame 생성
+        self.data = DataFrame(
+            { 'a_close': self.A_closeList,
+              'b_close': self.B_closeList},
+            columns=['a_close','b_close'],
+            index=self.dateList
+        )
+
+        # 괴리율 구하기
+        disparateRatio = []
+        for a_val,b_val in zip(self.A_closeList,self.B_closeList):
+            disparateRatio.append( (a_val - b_val)/a_val)
+        self.data.insert(len(self.data.columns), 'dr', disparateRatio)
+
+        # 괴리율 평균 구하기
+        avg = self.data['dr'].rolling(window=avg_period).mean()
+        self.data.insert(len(self.data.columns), 'dr_avg_250', avg)
+        # 하루 밀기
+        avg = avg.shift(1)
+
+        # 표준편차 구하기
+        #std = numpy.std(self.data['dr_avg_250'])
+        std = self.data['dr'].rolling(window=avg_period).std()
+        self.data.insert(len(self.data.columns), 'std_250', std)
+        # 하루 밀기
+        std = std.shift(1)
+
+        # 표준화
+        sz = []
+        for dr,avg,std in zip(self.data['dr'],self.data['dr_avg_250'],self.data['std_250']):
+            sz.append( (dr - avg) / std)
+        self.data.insert(len(self.data.columns), 'sz_250', sz)
+
+        self.last_avg = self.data.loc[self.data.index[len(self.data.index)-1]][3]
+        self.last_std = self.data.loc[self.data.index[len(self.data.index)-1]][4]
+        self.last_sz  = self.data.loc[self.data.index[len(self.data.index)-1]][5]
+        print('%s vs %s'%(self.A_name, self.B_name))
+        print(self.data.tail(30))
+        print("LAST avg = %f, std = %f, sz = %f"%(self.last_avg, self.last_std, self.last_sz))
+        
+
+    def GetSignalNow(self):
+        # 현재가격을 가져온다.
+        url = 'https://polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:%s'%self.A_code
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        self.A_price = json.loads(str(soup))['result']['areas'][0]['datas'][0]['nv']
+        url = 'https://polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:%s'%self.B_code
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        self.B_price = json.loads(str(soup))['result']['areas'][0]['datas'][0]['nv']
+
+        # 괴리율, 표준화
+        dr = (self.A_price - self.B_price)/self.A_price
+        sz = (dr - self.last_avg) / self.last_std
+
+        # Signal R
+        if (sz > self.SL_in_val): self.SL_r = True
+        else: self.SL_r = False
+        if (sz < self.LS_in_val): self.LS_r = True
+        else: self.LS_r = False
+        
+        # Short_Long Check
+        if (self.SL_r and sz < self.last_sz and dr > self.last_avg + (self.last_std * self.SL_in_val)): self.SL_in = True
+        else: self.SL_in = False
+        if (self.last_sz <= self.SL_out_val): self.SL_out = True
+        else: self.SL_out = False
+
+        # Long_Short Check
+        if (self.LS_r and sz > self.last_sz and dr < self.last_avg + (self.last_std * self.LS_in_val)): self.LS_in = True
+        else: self.LS_in = False
+        if (self.last_sz >= self.LS_out_val): self.LS_out = True
+        else: self.LS_out = False
+
+        print("Current Price : %d, %d (%f,%f)"%(self.A_price, self.B_price, dr, sz))
+        print("  ShortLong : R=%s, IN=%s, OUT=%s"%(self.SL_r, self.SL_in, self.SL_out))
+        print("  LongShort : R=%s, IN=%s, OUT=%s"%(self.SL_r, self.SL_in, self.SL_out))
+
--- main.py
+++ main.py
@@ -5,105 +5,17 @@
 # SZ : standardization
 # An close | Ap close | disparate ratio | DR AVG 250 | STD 250 | SZ | signal in | signal out
 
-import requests
-import sqlite3
-import json
-import numpy
-from pandas import Series, DataFrame
-from bs4 import BeautifulSoup
-#from datetime import date, time, datetime, timedelta
 import time
 import datetime
+from NPPair import NPPair
 
-class NPPair:
-    def __init__(self, code1, code2):
-        self.A_code = code1
-        self.B_code = code2
-        self.SL_in_val = 2
-        self.SL_out_val = 1
-        self.LS_in_val = -2
-        self.LS_out_val = -1.5
-        
-        self.dateList = []
-        self.A_closeList = []
-        self.B_closeList = []
-
-        yesterday = datetime.date.today() - datetime.timedelta(1)
-        
-        # A (보통주) 종가 가져오기
-        #req = requests.get('https://fchart.stock.naver.com/sise.nhn?symbol=%s&timeframe=day&count=260&requestType=0'%self.A_code)
-        req = requests.get('https://fchart.stock.naver.com/sise.nhn?symbol=%s&timeframe=day&startTime=%s&count=260&requestType=0'%(self.A_code, yesterday.strftime('%Y%m%d')))
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
-        self.A_name = soup.find('chartdata')['name']
-        for candle_element in soup.findAll('item'):
-            arr = candle_element['data'].split('|')
-            self.dateList.append(datetime.date(int(arr[0][0:4]), int(arr[0][4:6]), int(arr[0][6:9])))
-            self.A_closeList.append(int(arr[4]))
-
-        # B (우선주) 종가 가져오기
-        soup = BeautifulSoup(requests.get('https://fchart.stock.naver.com/sise.nhn?symbol=%s&timeframe=day&startTime=%s&count=260&requestType=0'%(self.B_code, yesterday.strftime('%Y%m%d'))).text, 'html.parser')
-        self.B_name = soup.find('chartdata')['name']
-        for candle_element in soup.findAll('item'):
-            self.B_closeList.append(int(candle_element['data'].split('|')[4]))
-
-        # DataFrame 생성
-        self.data = DataFrame(
-            { 'a_close': self.A_closeList,
-              'b_close': self.B_closeList},
-            columns=['a_close','b_close'],
-            index=self.dateList
-        )
-
-        # 괴리율 구하기
-        disparateRatio = []
-        for a_val,b_val in zip(self.A_closeList,self.B_closeList):
-            disparateRatio.append( (a_val - b_val)/a_val)
-        self.data.insert(len(self.data.columns), 'dr', disparateRatio)
-
-        # 괴리율 평균 구하기
-        avg = self.data['dr'].rolling(window=250).mean()
-        self.data.insert(len(self.data.columns), 'dr_avg_250', avg)
-
-        # 표준편차 구하기
-        #std = numpy.std(self.data['dr_avg_250'])
-        std = self.data['dr'].rolling(window=250).std()
-        self.data.insert(len(self.data.columns), 'std_250', std)
-
-        # 표준화
-        sz = []
-        for dr,avg,std in zip(self.data['dr'],self.data['dr_avg_250'],self.data['std_250']):
-            sz.append( (dr - avg) / std)
-        self.data.insert(len(self.data.columns), 'sz_250', sz)
-
-        #self.last_avg = 
-        print('%s vs %s'%(self.A_name, self.B_name))
-        #print(self.data)
-        print(self.data.tail(30))
-        
-
-    def GetSignalNow(self):
-        # 현재가격을 가져온다.
-        url = 'https://polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:%s'%self.A_code
-        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-        self.A_price = json.loads(str(soup))['result']['areas'][0]['datas'][0]['nv']
-        url = 'https://polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:%s'%self.B_code
-        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-        self.B_price = json.loads(str(soup))['result']['areas'][0]['datas'][0]['nv']
-
-        # 괴리율
-        dr = (self.A_price - self.B_price)/self.A_price
-
-        print("Current Price : %d, %d (%f)"%(self.A_price, self.B_price, dr))
-
-        # signal 처리
-
-
+def showSignal(val):
+    print(val)
 
 if __name__ == "__main__":
     #pair1 = NPPair('005930', '005935')
     # TODO: 종목을 설정으로 받자.
-    pair1 = NPPair('003490', '003495')
+    pair1 = NPPair('003490', '003495', 250)
 
     while 1:
         pair1.GetSignalNow()
