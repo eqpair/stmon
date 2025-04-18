badfe33501901609cebad0cffd76954a8ecaece0
eqpair <frmn3962@gmail.com>
2025-04-18 16:14:14 +0900

    update

--- trade_api.py
+++ trade_api.py
@@ -114,5 +114,5 @@ def get_trade_entries():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5002)
 
